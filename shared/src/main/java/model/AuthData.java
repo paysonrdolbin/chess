eb355531117ee50d0d7a46bf57341f524433fb3f
paysonrdolbin <pdolbin@byu.edu>
2025-05-20 21:22:24 -0600

    Finished AuthData class. Will likely need future getter functions.

--- shared/src/main/java/model/AuthData.java
+++ shared/src/main/java/model/AuthData.java
@@ -1,4 +1,11 @@
 package model;
 
 public class AuthData {
+    String authToken;
+    String username;
+
+    public AuthData(String authToken, String username) {
+        this.authToken = authToken;
+        this.username = username;
+    }
 }
