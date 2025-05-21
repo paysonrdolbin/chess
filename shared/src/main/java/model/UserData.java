306b557f498640315c378e834087bd32390ac901
paysonrdolbin <pdolbin@byu.edu>
2025-05-20 21:13:28 -0600

    Added some more empty files relating to storing things in memory. Finished the UserData class

--- shared/src/main/java/model/UserData.java
+++ shared/src/main/java/model/UserData.java
@@ -1,4 +1,13 @@
 package model;
 
-public class UserData {
+public class UserData{
+    private String username;
+    private String password;
+    private String email;
+
+    public UserData(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
 }
