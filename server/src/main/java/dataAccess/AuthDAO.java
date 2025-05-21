618d8420a99f16d48f863d373c60a409c8024f79
paysonrdolbin <pdolbin@byu.edu>
2025-05-20 21:02:28 -0600

    Made ClearRequest and ClearResponse classes. (they're empty)

--- server/src/main/java/dataAccess/AuthDAO.java
+++ server/src/main/java/dataAccess/AuthDAO.java
@@ -1,4 +1,7 @@
 package dataAccess;
 
 public class AuthDAO {
+    public void clear() {
+
+    }
 }
--- server/src/main/java/service/ClearService.java
+++ server/src/main/java/service/ClearService.java
@@ -2,9 +2,17 @@ package service;
 
 import dataAccess.GameDAO;
 import dataAccess.UserDAO;
+import dataAccess.AuthDAO;
+import response.ClearResponse;
+import request.ClearRequest;
+
 
 public class ClearService {
-    public Object clear(){
-
+    public ClearResponse clear(ClearRequest request){
+        GameDAO.clear();
+        UserDAO.clear():
+        AuthDAO.clear();
+        ClearResponse response = new ClearResponse();
+        return response;
     }
 }
