a9b3aafe70fc015a5ce77e53e6f6d2d68a76353d
paysonrdolbin <pdolbin@byu.edu>
2025-05-31 16:55:57 -0600

    Update GameDAO to handle DataAccess functions.

--- server/src/main/java/dataaccess/GameDAO.java
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -1,29 +1,50 @@
 package dataaccess;
 import memorydao.MemoryGameDAO;
 import model.GameData;
+import sqlDAO.SQLGameDAO;
 
 import java.util.ArrayList;
 
 public class GameDAO {
-    private final static MemoryGameDAO GAME_DB = new MemoryGameDAO();
+    private final static SQLGameDAO GAME_DB = new SQLGameDAO();
 
     public static void clear(){
-        GAME_DB.clear();
+        try{
+            GAME_DB.clear();
+        } catch (DataAccessException e) {
+            throw new IllegalArgumentException("Error: Database was unable to clear games.");
+        }
     }
 
     public static void create(GameData gameData){
-        GAME_DB.create(gameData);
+        try{
+            GAME_DB.create(gameData);
+        } catch (DataAccessException e) {
+            throw new IllegalArgumentException("Error: Database was unable to create game");
+        }
     }
 
     public static GameData get(int gameID){
-        return GAME_DB.get(gameID);
+        try{
+            return GAME_DB.get(gameID);
+        } catch (DataAccessException e) {
+            throw new IllegalArgumentException("Error: Database was unable to retrieve game");
+        }
     }
 
     public static ArrayList<GameData> list(){
-        return GAME_DB.list();
+        try{
+            return GAME_DB.list();
+        } catch (DataAccessException e) {
+            throw new IllegalArgumentException("Error: Database was unable to generate list of current games");
+        }
     }
 
     public static void update(GameData data){
-        GAME_DB.update(data);
+        try{
+            GAME_DB.update(data);
+        } catch (DataAccessException e) {
+            throw new IllegalArgumentException("Error: Database was unable to update the current game");
+        }
     }
 }
