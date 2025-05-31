31b850b202419d04d68c96f4b9438e8a33351249
paysonrdolbin <pdolbin@byu.edu>
2025-05-31 12:23:56 -0600

    Adapted UserDAO to handle DataAccessExceptions

--- server/src/main/java/dataaccess/UserDAO.java
+++ server/src/main/java/dataaccess/UserDAO.java
@@ -1,23 +1,40 @@
 package dataaccess;
 
 import memorydao.MemoryUserDAO;
+import sqlDAO.SQLUserDAO;
 import model.UserData;
 
+import javax.xml.crypto.Data;
+
 public class UserDAO {
-    private final static MemoryUserDAO USERS = new MemoryUserDAO();
+    private final static SQLUserDAO USERS = new SQLUserDAO();
     public static void clear(){
-        USERS.clear();
+        try {
+            USERS.clear();
+        } catch (DataAccessException e){
+            throw new IllegalArgumentException("Error: Unable to clear user db");
+        }
+
     }
     public static void add(UserData data){
-        USERS.add(data);
+        try{
+            USERS.add(data);
+        } catch (DataAccessException e){
+            throw new IllegalArgumentException("Error: Unable to add user");
+        }
     }
 
     public static UserData getUserData(String username){
-        UserData data = USERS.get(username);
-        if(data == null){
-            throw new IllegalArgumentException("Error: unauthorized");
+        try {
+            UserData data = USERS.get(username);
+            if(data == null){
+                throw new IllegalArgumentException("Error: unauthorized");
+            }
+            return data;
+        } catch(DataAccessException e){
+            throw new IllegalArgumentException("Error: Unable to get user");
         }
-        return data;
+
     }
 
 }
