f44cb0efe7b374e75ed5b8cf9bafaccb7fda8397
paysonrdolbin <pdolbin@byu.edu>
2025-06-03 12:08:08 -0600

    Wrote user  SQLdb tests.

--- server/src/test/java/service/UserServiceTest.java
+++ server/src/test/java/service/UserServiceTest.java
@@ -2,16 +2,11 @@ package service;
 
 import dataaccess.AuthDAO;
 import dataaccess.DataAccessException;
-import model.UserData;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import request.*;
-import response.CreateGameResponse;
-import response.ListGamesResponse;
 import response.LoginResponse;
 import response.RegisterResponse;
-import service.GameService;
-import model.GameData;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class UserServiceTest {
