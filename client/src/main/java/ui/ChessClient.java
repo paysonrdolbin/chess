6564cc82171117f41bb8b5794d0fba9957c1ea2e
paysonrdolbin <pdolbin@byu.edu>
2025-06-04 21:06:59 -0600

    Created ChessClient and ServerFacade files

---
+++ client/src/main/java/ui/ChessClient.java
@@ -0,0 +1,13 @@
+package ui;
+
+import java.util.Scanner;
+
+public class ChessClient {
+    private final Scanner Scanner = new Scanner(System.in);
+
+    public void run(){
+        System.out.println("♕ Welcome to Payson's Chess Server! ♕\nType 'help' to get started.");
+
+
+    }
+}
