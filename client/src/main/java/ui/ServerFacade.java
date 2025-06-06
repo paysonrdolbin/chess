72ae073caa248b56417b673e757c70691e94a118
paysonrdolbin <pdolbin@byu.edu>
2025-06-05 18:27:14 -0600

    Created framework for ServerFacade

--- client/src/main/java/ui/ServerFacade.java
+++ client/src/main/java/ui/ServerFacade.java
@@ -1,4 +1,28 @@
 package ui;
 
+import model.ListGameShortResponse;
+import java.util.ArrayList;
+
 public class ServerFacade {
+    private final String authToken = null;
+
+
+    public int create(String gameName){
+        return 1234;
+    }
+    public void logout(){
+
+    }
+    public ArrayList<ListGameShortResponse> list(){
+        ArrayList<ListGameShortResponse> games = new ArrayList<ListGameShortResponse>();
+        return games;
+    }
+
+    public void register(String username, String password, String email){}
+
+    public void login(String username, String password){}
+
+    public void observe(int gameID){}
+
+    public void join(int gameID, String teamColor){}
 }
