a36e8d4fd9b802644b0b67a09eb482c1e4043fb9
paysonrdolbin <pdolbin@byu.edu>
2025-06-08 16:36:19 -0600

    Added basic main client functionality.

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,11 +1,9 @@
 import chess.*;
+import ui.ChessClient;
 
 public class Main {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
-
-
-
+        ChessClient client = new ChessClient("http://localhost:8080");
+        client.run();
     }
 }
