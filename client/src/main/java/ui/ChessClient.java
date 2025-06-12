b12b1c2d7b8c5cff5559ac48060e299c309c44bb
paysonrdolbin <pdolbin@byu.edu>
2025-06-12 01:22:59 -0600

    Made the team color a variable of the client to ease gameplay functionality. Added gameplay functions without websocket capabilities. Added the logic of move functions without websocket.

--- client/src/main/java/ui/ChessClient.java
+++ client/src/main/java/ui/ChessClient.java
@@ -1,6 +1,6 @@
 package ui;
 
-import chess.ChessBoard;
+import chess.*;
 import exception.ResponseException;
 import model.ListGameShortResponse;
 import response.ListGamesResponse;
@@ -13,11 +13,16 @@ public class ChessClient {
     private final ServerFacade serverFacade;
     private boolean loggedIn = false;
     private boolean clientRunning = true;
+    private boolean inGameplay = false;
     private ListGamesResponse gameListObject = null;
     private ArrayList<ListGameShortResponse> gameList = new ArrayList<>();
+    private boolean isWhite = false;
+    private int gameID = 0;
+
     private final String preLoginMessage = """
-                register [username] [password] [email] - Creates
-                 an account so you can play (Also logs you in :)
+                Here are your options:
+                
+                register [username] [password] [email] - Createsan account so you can play (Also logs you in :)
                 login [username] [password] - Logs you into your account.
                 quit - Quits your session.
                 help - See this message again.
@@ -32,69 +37,211 @@ public class ChessClient {
 
         while(clientRunning) {
             // pre-login
-            while (!loggedIn) {
-                System.out.println(">>> ");
-                String input = SCANNER.nextLine();
-                String[] words = input.trim().split("\\s+");
-
-                switch (words[0].toLowerCase()) {
-                    case "help":
-                        handlePreLoginHelp();
-                        break;
-
-                    case "register":
-                        handleRegister(words);
-                        break;
-
-                    case "login":
-                        handleLogin(words);
-                        break;
-
-                    case "quit":
-                        handleQuit();
-                        return;
-                }
-            }
-
+            preLogin();
             // post-login
-            while (loggedIn) {
-                System.out.println(">>> ");
-                String input = SCANNER.nextLine();
-                String[] words = input.trim().split("\\s+");
+            postLogin();
+        }
+    }
 
-                switch (words[0].toLowerCase()) {
+    public void preLogin(){
+        while (!loggedIn) {
+            System.out.println(">>> ");
+            String input = SCANNER.nextLine();
+            String[] words = input.trim().split("\\s+");
 
-                    case "help":
-                        handlePostLoginHelp();
-                        break;
+            switch (words[0].toLowerCase()) {
+                case "help":
+                    handlePreLoginHelp();
+                    break;
 
-                    case "create":
-                        handleCreate(words);
-                        break;
+                case "register":
+                    handleRegister(words);
+                    loggedIn = true;
+                    postLogin();
+                    break;
 
-                    case "join":
-                        handleJoin(words);
-                        break;
+                case "login":
+                    handleLogin(words);
+                    loggedIn = true;
+                    break;
 
-                    case "list":
-                        handleList();
-                        break;
-
-                    case "observe":
-                        handleObserve(words);
-                        break;
-
-                    case "quit":
-                        handleQuit();
-                        return;
-
-                    case "logout":
-                        handleLogout();
-                        break;
-                }
-
+                case "quit":
+                    handleQuit();
+                    return;
             }
         }
+    }
+
+    public void postLogin(){
+        while (loggedIn) {
+            System.out.println(">>> ");
+            String input = SCANNER.nextLine();
+            String[] words = input.trim().split("\\s+");
+
+            switch (words[0].toLowerCase()) {
+
+                case "help":
+                    handlePostLoginHelp();
+                    break;
+
+                case "create":
+                    handleCreate(words);
+                    break;
+
+                case "join":
+                    handleJoin(words);
+                    inGameplay = true;
+                    gameplay();
+                    break;
+
+                case "list":
+                    handleList();
+                    break;
+
+                case "observe":
+                    handleObserve(words);
+                    inGameplay = true;
+                    gameplay();
+                    break;
+
+                case "quit":
+                    handleQuit();
+                    return;
+
+                case "logout":
+                    handleLogout();
+                    loggedIn = false;
+                    break;
+            }
+        }
+    }
+
+    public void gameplay(){
+        while(inGameplay){
+            System.out.println(">>> ");
+            String input = SCANNER.nextLine();
+            String[] words = input.trim().split("\\s+");
+
+            switch(words[1].toLowerCase()){
+                case "help":
+                    handleGameplayHelp();
+                    break;
+                case "redraw":
+                    ChessGame game = new ChessGame(); // replace with getting the chessboard from the server.
+                    ChessBoardUI.main(game, isWhite, null);
+                    break;
+                case "highlight":
+                    ChessGame game1 = new ChessGame(); // use the current board?? get board?? -- websocket
+                    ChessPosition coord = decodePosition(words[2]);
+                    ChessBoardUI.main(game1, isWhite, coord);
+                    break;
+                case "move":
+                    handleMove(words);
+                    break;
+            }
+
+
+        }
+
+    }
+
+
+
+
+    private ChessPosition decodePosition(String pos){
+        int row = Integer.parseInt(pos.substring(1));
+        int col;
+        switch(pos.charAt(0)){
+            case 'a': col = 1; break;
+            case 'b': col = 2; break;
+            case 'c': col = 3; break;
+            case 'd': col = 4; break;
+            case 'e': col = 5; break;
+            case 'f': col = 6; break;
+            case 'g': col = 7; break;
+            case 'h': col = 8; break;
+            default: col = -1;
+        }
+        return new ChessPosition(row, col);
+    }
+
+    private void handleMove(String[] words){
+        try {
+            ChessGame game1 = new ChessGame(); // use the current game from websocket
+            ChessBoard board = game1.getBoard();
+            ChessPosition startPos = decodePosition(words[1]);
+            ChessPosition endPos = decodePosition(words[2]);
+            ChessMove move;
+            // deal with promotion pieces
+
+            // if the piece type is a pawn
+            if (board.getPiece(startPos).equals(ChessPiece.PieceType.PAWN)) {
+                // if the end position is on the respective final row
+                if ((endPos.getRow() == 8 && isWhite == true) || (endPos.getRow() == 1 && isWhite == false)) {
+                    // prompts the user for a promotion piece
+                    System.out.println("Please choose a new piece type for your pawn");
+                    System.out.println(">>> ");
+                    String input = SCANNER.nextLine();
+                    String[] words1 = input.trim().split("\\s");
+                    ChessPiece.PieceType promotion = decodePieceType(words1[0].toLowerCase());
+                    // they didn't give a right piece type -- provide options???
+                    if (promotion.equals(null)) {
+                        throw new IllegalArgumentException("Please provide a valid piece for promotion."); // fix this
+                    }
+                    move = new ChessMove(startPos, endPos, promotion);
+                } // otherwise promotion = null
+                else {
+                    move = new ChessMove(startPos, endPos, null);
+                } // otherwise promotion = null
+            } else {
+                move = new ChessMove(startPos, endPos, null);
+            }
+            game1.makeMove(move);
+            // websocket update board with new move.
+            ChessBoardUI.main(game1, isWhite, null);
+        } catch(InvalidMoveException e){
+            System.out.println("Please provide a valid chess move in a similar format 'e4g6'.");
+            System.out.println("Use 'highlight' to see valid piece moves.");
+        }
+    }
+
+    private ChessPiece.PieceType decodePieceType(String input){
+        ChessPiece.PieceType promotion = null;
+        switch(input){
+            case "queen":
+                promotion = ChessPiece.PieceType.QUEEN;
+                break;
+            case "bishop":
+                promotion = ChessPiece.PieceType.BISHOP;
+                break;
+            case "knight":
+                promotion = ChessPiece.PieceType.KNIGHT;
+                break;
+            case "rook":
+                promotion = ChessPiece.PieceType.ROOK;
+                break;
+        }
+        return promotion;
+    }
+
+
+    private void handleGameplayHelp(){
+        System.out.println("""
+        Here are your options:
+        
+        redraw - Draws an updated chess board. 
+        leave - Removes you from the current game. 
+        move [chessmove] - Excecutes the given chess move if valid. (Chess move notation is required. ex: 'e4g6')
+        resign - Forfeit the current game. 
+        highlight [chess position] - Highlights all the valid moves for the piece at the given position.
+        help - See this message again.
+        """);
+    }
+
+
+    private void handleResignation(){
+        System.out.println("You have resigned. You forfeit this match.");
+        // update the game board?
     }
 
     private void handlePreLoginHelp() {
@@ -105,7 +252,6 @@ public class ChessClient {
         if(words.length > 3) {
             try {
                 serverFacade.register(words[1], words[2], words[3]);
-                loggedIn = true;
                 System.out.println("Registered! You are now logged in.");
             } catch (ResponseException e) {
                 switch(e.statusCode()){
@@ -120,6 +266,8 @@ public class ChessClient {
                         break;
                 }
             }
+        } else {
+            System.out.println("Please provide a username, password, and email.");
         }
     }
 
@@ -128,7 +276,6 @@ public class ChessClient {
             try {
                 serverFacade.login(words[1], words[2]);
                 System.out.println("Login Success.");
-                loggedIn = true;
             } catch (ResponseException e) {
                 switch(e.statusCode()){
                     case 400:
@@ -196,16 +343,21 @@ public class ChessClient {
                 // if an ID has been provided, observe the game.
                 try{
                     int gameIndex = Integer.parseInt(words[1]) - 1;
-                    System.out.println("Please type 'join' followed by the ID and white/black.");
                     if(gameIndex >= gameList.size() || gameIndex < 0){
                         System.out.println("Please enter a valid game ID.");
                     } else {
-                        int gameID = gameList.get(gameIndex).getGameID();
+                        gameID = gameList.get(gameIndex).getGameID();
+                        if(words[2].toLowerCase().equals("white")){
+                            isWhite = true;
+                        } else if(words[2].toLowerCase().equals("black")){
+                            isWhite = false;
+                        } else{
+                            throw new IllegalArgumentException("Error: bad request");
+                        }
                         serverFacade.join(gameID, words[2]);
                         System.out.println("Game joined!");
-                        ChessBoard board = new ChessBoard();
-                        board.resetBoard();
-                        ChessBoardUI.main(board);
+                        ChessGame game = new ChessGame(); // use websocket to get the game?
+                        ChessBoardUI.main(game, isWhite, null);
                     }
                 } catch (ResponseException e) {
                     switch(e.statusCode()){
@@ -220,6 +372,7 @@ public class ChessClient {
                             break;
                         case 500:
                             System.out.println(e.getMessage());
+                            break;
                         default:
                             System.out.println("Please type 'join' followed by the game ID number and white/black.");
                     }
@@ -256,12 +409,12 @@ public class ChessClient {
                 // if an ID has been provided, join the game.
                 try {
                     int gameIndex = Integer.parseInt(words[1]) - 1;
-                    int gameID = gameList.get(gameIndex).getGameID();
+                    gameID = gameList.get(gameIndex).getGameID();
+                    Boolean isWhite = true;
                     serverFacade.observe(gameID);
                     System.out.println("You are now observing!");
-                    ChessBoard board = new ChessBoard();
-                    board.resetBoard();
-                    ChessBoardUI.main(board);
+                    ChessGame game = new ChessGame();
+                    ChessBoardUI.main(game, isWhite, null);
                 } catch (Exception e) {
                     System.out.println("Please type 'observe', followed by the game ID number.");
                 }
@@ -278,14 +431,13 @@ public class ChessClient {
             // no ID provided
             gameListObject = list(gameListObject, gameList);
             gameList = gameListObject.getGames();
-            System.out.println("Please type 'join' followed by the game ID number.");
+            System.out.println("Please type 'observe' followed by the game ID number.");
         }
     }
 
     private void handleLogout() {
         try{
             serverFacade.logout();
-            loggedIn = false;
             System.out.println("Logout Successful");
         } catch (ResponseException e) {
             System.out.println("Server Error");
@@ -301,8 +453,19 @@ public class ChessClient {
                 int n = 1;
                 while (n <= games.size()) {
                     ListGameShortResponse game = games.get(n - 1);
+                    String whiteUsername;
+                    if(game.getWhiteUsername() == null){
+                        whiteUsername = "available";
+                    } else{
+                        whiteUsername = game.getWhiteUsername();
+                    } String blackUsername;
+                    if(game.getBlackUsername() == null){
+                        blackUsername = "available";
+                    } else{
+                        blackUsername = game.getBlackUsername();
+                    }
                     String statement = n + ". " + game.getGameName() + ", ID: " + game.getGameID() + " - White: "
-                            + game.getWhiteUsername() + " Black: " + game.getBlackUsername();
+                            + whiteUsername + " Black: " + blackUsername;
                     System.out.println(statement);
                     n++;
                 }
